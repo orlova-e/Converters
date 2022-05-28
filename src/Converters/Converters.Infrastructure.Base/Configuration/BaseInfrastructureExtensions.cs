fd2b9e901b31b622734751da4e542880fd264066
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:03:23 +0300

    add base infrastructure extensions

---
+++ src/Converters/Converters.Infrastructure.Base/Configuration/BaseInfrastructureExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Converters.Infrastructure.Base.Configuration;
+
+public static class BaseInfrastructureExtensions
+{
+    public static IServiceCollection AddBaseInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services
+            .AddDbContext<Context>(option => option.UseSqlite(connectionString));
+
+        return services;
+    }
+}
