6f6537e2ed3baba2f2764de1eebdf903e57ec4a2
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:03:38 +0300

    add common infrastructure extensions

---
+++ src/Converters/Converters.Infrastructure.DataAccess/Configuration/CommonInfrastructureExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Converters.Infrastructure.DataAccess.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Converters.Infrastructure.DataAccess.Configuration;
+
+public static class CommonInfrastructureExtensions
+{
+    public static IServiceCollection AddCommonInfrastructure(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IRepository, Repository>();
+
+        return services;
+    }
+}
