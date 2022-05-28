cb4ac607aa73a3117d8ae99f87ddbe9839282ae7
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:02:30 +0300

    add DbContext implementation

---
+++ src/Converters/Converters.Infrastructure.Base/Configuration/Context.cs
@@ -0,0 +1,16 @@
+﻿using Converters.Infrastructure.Base.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Converters.Infrastructure.Base.Configuration;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ConvertationConfiguration).Assembly);
+    }
+}
