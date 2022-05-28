b0051a89ac7656e3b1825a948e282f40565e0d82
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:00:44 +0300

    add domain models

---
+++ src/Converters/Converters.Domain/Entities/Convertation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Converters.Domain.Interfaces;
+
+namespace Converters.Domain.Entities
+{
+    public class Convertation : IDomainEntity, IHistorical
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Deleted { get; set; }
+        public string Name { get; set; }
+        public Guid JsonFileId { get; set; }
+        public File JsonFile { get; set; }
+        public Guid XmlFileId { get; set; }
+        public File XmlFile { get; set; }
+    }
+}
---
+++ src/Converters/Converters.Domain/Entities/File.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Converters.Domain.Interfaces;
+
+namespace Converters.Domain.Entities
+{
+    public class File : IDomainEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
