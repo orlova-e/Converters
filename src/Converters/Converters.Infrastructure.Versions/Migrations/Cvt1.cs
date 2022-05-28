6a9df9627a9d3839da3b9b46dc5538a132589693
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:11:05 +0300

    add database migrations

---
+++ src/Converters/Converters.Infrastructure.Versions/Migrations/Cvt1.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Converters.Infrastructure.Versions.Migrations;
+
+[Migration(1, "add files table")]
+public class Cvt1 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Table("files")
+            .WithColumn("id").AsGuid().PrimaryKey()
+            .WithColumn("isdeleted").AsBoolean().NotNullable()
+            .WithColumn("data").AsBinary().NotNullable();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Table("files");
+    }
+}
---
+++ src/Converters/Converters.Infrastructure.Versions/Migrations/Cvt2.cs
@@ -0,0 +1,37 @@
+﻿using FluentMigrator;
+
+namespace Converters.Infrastructure.Versions.Migrations;
+
+[Migration(2, "add convertations table")]
+public class Cvt2 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Table("convertations")
+            .WithColumn("id").AsGuid().PrimaryKey()
+            .WithColumn("isdeleted").AsBoolean().NotNullable()
+            .WithColumn("name").AsString().NotNullable()
+            .WithColumn("created").AsDateTime().NotNullable()
+            .WithColumn("updated").AsDateTime().Nullable()
+            .WithColumn("deleted").AsDateTime().Nullable()
+            .WithColumn("jsonfileid").AsGuid().NotNullable()
+            .WithColumn("xmlfileid").AsGuid().NotNullable();
+        
+        Create
+            .ForeignKey("FK_convertations_jsonfileid")
+            .FromTable("convertations").ForeignColumn("jsonfileid")
+            .ToTable("files").PrimaryColumn("id");
+        
+        Create
+            .ForeignKey("FK_convertations_xmlfileid")
+            .FromTable("convertations").ForeignColumn("xmlfileid")
+            .ToTable("files").PrimaryColumn("id");
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Table("convertations");
+    }
+}
