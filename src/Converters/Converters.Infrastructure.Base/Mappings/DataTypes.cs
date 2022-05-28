7f8a7c91232a3ebd4a324ba2ad56288f3ed152ab
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:01:53 +0300

    add DataTypes

---
+++ src/Converters/Converters.Infrastructure.Base/Mappings/DataTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Converters.Infrastructure.Base.Mappings;
+
+internal static class DataTypes
+{
+    public const string Uuid = "text";
+    public const string Boolean = "boolean";
+    public const string Blob = "blob";
+    public const string Text = "text";
+    public const string Integer = "integer";
+    public const string TimestampWithoutTimeZone = "text";
+}
