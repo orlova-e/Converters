5f106987f1d90c13a50b679408baa6742cf29835
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-28 12:01:15 +0300

    add specifications

---
+++ src/Converters/Converters.Domain.Services/Specs/Domain/Common.cs
@@ -0,0 +1,15 @@
+﻿using Converters.Domain.Interfaces;
+
+namespace Converters.Domain.Services.Specs.Domain;
+
+public static class Common
+{
+    public static Spec<T> NotDeleted<T>() where T : class, IDomainEntity
+        => new(x => !x.IsDeleted);
+    public static Spec<T> NotDeleted<T>(Guid id) where T : class, IDomainEntity
+        => new(x => !x.IsDeleted & x.Id == id);
+    public static Spec<T> Deleted<T>() where T : class, IDomainEntity
+        => new(x => x.IsDeleted);
+    public static Spec<T> ById<T>(Guid id) where T : class, IDomainEntity
+        => new(x => x.Id == id);
+}
---
+++ src/Converters/Converters.Domain.Services/Specs/Domain/Convertations.cs
@@ -0,0 +1,6 @@
+﻿namespace Converters.Domain.Services.Specs.Domain;
+
+public static class Convertations
+{
+    public static Spec<Entities.Convertation> ByName(string name) => new(x => x.Name == name);
+}
