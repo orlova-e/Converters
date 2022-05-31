50c93e7e1b6ede0e07fd7718f2cb8e08a4131d03
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-31 22:09:36 +0300

    add ConverterFactory.cs

---
+++ src/Converters/Converters.Services/Implementation/ConverterFactory.cs
@@ -0,0 +1,18 @@
+﻿using Converters.Services.Interfaces;
+
+namespace Converters.Services.Implementation;
+
+public static class ConverterFactory
+{
+    public static IConverter GetConverter(string fileName)
+    {
+        var extension = Path.GetExtension(fileName);
+        
+        return extension switch
+        {
+            ".xml" => new XmlToJsonConverter(),
+            ".json" => new JsonToXmlConverter(),
+            { } => throw new ArgumentException($"No converters for extension '{extension}' can be provided")
+        };
+    }
+}
