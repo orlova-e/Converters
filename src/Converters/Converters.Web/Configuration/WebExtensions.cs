2afc9a739f311185125f0b598d850eabcfb6bfed
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-31 22:13:46 +0300

    add AddWebServices(...) extension method for IServiceCollection

---
+++ src/Converters/Converters.Web/Configuration/WebExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Converters.Web.Hubs;
+using Converters.Web.Services.Implementation;
+using Converters.Web.Services.Interfaces;
+using FluentValidation.AspNetCore;
+using MediatR;
+
+namespace Converters.Web.Configuration;
+
+public static class WebExtensions
+{
+    public static IServiceCollection AddWebServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var hubOptions = configuration
+            .GetSection(nameof(HubOptions))
+            .Get<HubOptions>();
+
+        services
+            .AddSignalR()
+            .AddHubOptions<ConvertersHub>(options =>
+            {
+                options.ClientTimeoutInterval = TimeSpan.FromSeconds(hubOptions.ClientTimeoutInterval);
+                options.KeepAliveInterval = TimeSpan.FromSeconds(hubOptions.KeepAliveInterval);
+            });
+        
+        services
+            .AddMediatR(typeof(Program).Assembly)
+            .AddFluentValidation(f =>
+                f.RegisterValidatorsFromAssemblyContaining<Program>())
+            .AddTransient<ITranslator, Translator>()
+            .AddAutoMapper(typeof(Program).Assembly);
+
+        return services;
+    }
+}
