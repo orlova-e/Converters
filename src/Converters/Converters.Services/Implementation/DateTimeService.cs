57d09098476d3525db0f753dc8932f49175c2fe3
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-31 22:09:27 +0300

    add IDateTimeService and its implementation

---
+++ src/Converters/Converters.Services/Implementation/DateTimeService.cs
@@ -0,0 +1,18 @@
+﻿using Converters.Domain.Interfaces;
+using Converters.Services.Interfaces;
+
+namespace Converters.Services.Implementation;
+
+internal class DateTimeService : IDateTimeService
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+    
+    public void Created<T>(T entity) where T : class, IHistorical
+        => entity.Created = this.UtcNow;
+
+    public void Updated<T>(T entity) where T : class, IHistorical
+        => entity.Updated = this.UtcNow;
+
+    public void Deleted<T>(T entity) where T : class, IHistorical
+        => entity.Deleted = this.UtcNow;
+}
---
+++ src/Converters/Converters.Services/Interfaces/IDateTimeService.cs
@@ -0,0 +1,11 @@
+﻿using Converters.Domain.Interfaces;
+
+namespace Converters.Services.Interfaces;
+
+public interface IDateTimeService
+{
+    DateTime UtcNow { get; }
+    void Created<T>(T entity) where T : class, IHistorical;
+    void Updated<T>(T entity) where T : class, IHistorical;
+    void Deleted<T>(T entity) where T : class, IHistorical;
+}
