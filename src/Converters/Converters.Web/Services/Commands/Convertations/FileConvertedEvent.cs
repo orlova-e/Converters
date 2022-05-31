ec07b8ce0ba65224dfa4407cccc3adea106e94de
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-31 22:12:45 +0300

    add FileConvertedEvent.cs and its handler

---
+++ src/Converters/Converters.Web/Services/Commands/Convertations/FileConvertedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Converters.Web.Models.Convertations;
+using MediatR;
+
+namespace Converters.Web.Services.Commands.Convertations;
+
+public class FileConvertedEvent : INotification
+{
+    public GetConvertationDto Dto { get; }
+
+    public FileConvertedEvent(GetConvertationDto dto)
+    {
+        Dto = dto;
+    }
+}
---
+++ src/Converters/Converters.Web/Services/Commands/Convertations/FileConvertedHandler.cs
@@ -0,0 +1,20 @@
+﻿using Converters.Web.Hubs;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Converters.Web.Services.Commands.Convertations;
+
+public class FileConvertedHandler : INotificationHandler<FileConvertedEvent>
+{
+    private readonly IHubContext<ConvertersHub> _hubContext;
+
+    public FileConvertedHandler(IHubContext<ConvertersHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+    
+    public Task Handle(FileConvertedEvent notification, CancellationToken cancellationToken)
+    {
+        return _hubContext.Clients.All.SendAsync("fileConverted", notification.Dto, cancellationToken);
+    }
+}
