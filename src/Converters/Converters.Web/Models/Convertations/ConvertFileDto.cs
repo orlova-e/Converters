ba70eea93c22701fe3c4264e1e41538bf6a991c6
Elizaveta Orlova <orlovaev11@gmail.com>
2022-05-31 22:11:10 +0300

    add dto

---
+++ src/Converters/Converters.Web/Models/Convertations/ConvertFileDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Converters.Web.Models.Convertations;
+
+public class ConvertFileDto
+{
+    public IFormFile File { get; init; }
+}
---
+++ src/Converters/Converters.Web/Models/Convertations/GetConvertationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Converters.Web.Models.Convertations;
+
+public class GetConvertationDto
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public DateTime Created { get; init; }
+    public string JsonDownloadPath { get; init; }
+    public string XmlDownloadPath { get; init; }
+}
