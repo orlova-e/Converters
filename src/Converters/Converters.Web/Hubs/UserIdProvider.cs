460710bdffea11ff20f012fb75c1266646ba3b9c
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 12:48:02 +0300

    add UserIdProvider.cs

---
+++ src/Converters/Converters.Web/Hubs/UserIdProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Converters.Web.Hubs;
+
+public class UserIdProvider : IUserIdProvider
+{
+    public string? GetUserId(HubConnectionContext connection)
+    {
+        return connection.GetHttpContext().Session.Id;
+    }
+}
