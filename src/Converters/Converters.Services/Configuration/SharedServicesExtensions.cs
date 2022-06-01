f2cc14bf96caca536cbeab019cd3366fa098ba05
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 12:47:21 +0300

    add SharedServicesExtensions.cs

---
+++ src/Converters/Converters.Services/Configuration/SharedServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Converters.Services.Implementation;
+using Converters.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Converters.Services.Configuration;
+
+public static class SharedServicesExtensions
+{
+    public static IServiceCollection AddSharedServices(this IServiceCollection services)
+    {
+        services
+            .AddSingleton<IDateTimeService, DateTimeService>()
+            .AddSingleton<IFileManager, FileManager>();
+
+        return services;
+    }
+}
