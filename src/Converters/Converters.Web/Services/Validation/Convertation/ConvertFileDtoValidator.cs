2b9988066aa8a822de5c21371f6b3b6d08b3d6c4
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 12:51:02 +0300

    add validators

---
+++ src/Converters/Converters.Web/Services/Validation/Convertation/ConvertFileDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using Converters.Web.Configuration;
+using Converters.Web.Models.Convertations;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace Converters.Web.Services.Validation.Convertation;
+
+public class ConvertFileDtoValidator : AbstractValidator<ConvertFileDto>
+{
+    private readonly IOptions<ValidationOptions> _options;
+
+    public ConvertFileDtoValidator(IOptions<ValidationOptions> options)
+    {
+        _options = options;
+        
+        RuleFor(x => x.File)
+            .Must(IsExtensionValid);
+    }
+
+    private bool IsExtensionValid(IFormFile file)
+    {
+        var extension = Path.GetExtension(file.FileName);
+        return _options.Value.AllowedTypes.Contains(extension);
+    }
+}
---
+++ src/Converters/Converters.Web/Services/Validation/Convertation/DownloadConvertationDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Converters.Domain.Conditions;
+using Converters.Infrastructure.DataAccess;
+using Converters.Web.Models.Convertations;
+using FluentValidation;
+using Notes.Web.API.Services.Validation.Helpers;
+
+namespace Converters.Web.Services.Validation.Convertation;
+
+public class DownloadConvertationDtoValidator : AbstractValidator<DownloadConvertationDto>
+{
+    public DownloadConvertationDtoValidator(IRepository repository)
+    {
+        RuleFor(x => x.Id)
+            .Exists<Domain.Entities.Convertation, DownloadConvertationDto>(repository);
+
+        RuleFor(x => x.Type)
+            .Must(IsTypeValid);
+    }
+
+    private bool IsTypeValid(FileType type)
+        => Enum.IsDefined(type);
+}
