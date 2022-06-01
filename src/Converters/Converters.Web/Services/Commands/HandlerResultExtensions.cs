1207985d3cd06ec3dd32a3248dc0d6bc2d041115
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 12:49:30 +0300

    add HandlerResult.cs and extensions

---
+++ src/Converters/Converters.Web/Services/Commands/HandlerResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Converters.Web.Models.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Converters.Web.Services.Commands;
+
+public static class HandlerResultExtensions
+{
+    public static IActionResult Unwrap<T>(
+        this ControllerBase controller,
+        HandlerResult<T> result,
+        string action = "",
+        string controllerRoute = "Index")
+    {
+        if (result is FileContent fileContext)
+            return new FileStreamResult(fileContext.Content, fileContext.ContentType)
+            {
+                FileDownloadName = fileContext.FileName
+            };
+        
+        return result.Status switch
+        {
+            OperationStatus.Success => controller.RedirectToAction(action, controllerRoute),
+            OperationStatus.InternalError => controller.Problem(result.FailureMessage),
+            { } => controller.Problem()
+        };
+    }
+}
