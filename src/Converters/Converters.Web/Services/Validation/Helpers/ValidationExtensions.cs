65b8b1784edf434535f741cbb5d4ac0bf72022db
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 12:50:43 +0300

    add validation helpers

---
+++ src/Converters/Converters.Web/Services/Validation/Helpers/ValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Converters.Domain.Interfaces;
+using Converters.Infrastructure.DataAccess;
+using FluentValidation;
+
+namespace Notes.Web.API.Services.Validation.Helpers;
+
+public static class ValidationExtensions
+{
+    public static IRuleBuilderOptions<TEditorDto, Guid> Exists<TEntity, TEditorDto>(
+        this IRuleBuilder<TEditorDto, Guid> ruleBuilder, 
+        IRepository repository)
+        where TEntity : class, IDomainEntity
+    {
+        return ruleBuilder.SetAsyncValidator(new ExistsValidator<TEntity, TEditorDto>(repository));
+    }
+}
