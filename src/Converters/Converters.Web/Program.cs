d454c7e2129fd73a72ef93d00d9cd9c6920ecc10
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 13:08:50 +0300

    add Program

--- src/Converters/Converters.Web/Program.cs
+++ src/Converters/Converters.Web/Program.cs
@@ -1,6 +1,43 @@
+using Converters.Infrastructure.Base.Configuration;
+using Converters.Infrastructure.DataAccess.Configuration;
+using Converters.Services.Configuration;
+using Converters.Web.Configuration;
+using Converters.Web.Hubs;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services
+    .AddBaseInfrastructure(builder.Configuration)
+    .AddCommonInfrastructure()
+    .AddSharedServices()
+    .AddWebServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app
+    .UseForwardedHeaders(new ForwardedHeadersOptions 
+        {ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto})
+    .UseHttpsRedirection()
+    .UseSession()
+    .UseStaticFiles()
+    .UseRouting()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapHub<ConvertersHub>("/hubs");
+        endpoints.MapDefaultControllerRoute();
+    });
 
 app.Run();
