7e077ec2d6eaaf3f4bc710abf5626e64b479ff0d
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-01 12:48:33 +0300

    add DownloadConvertationDto.cs

---
+++ src/Converters/Converters.Web/Models/Convertations/DownloadConvertationDto.cs
@@ -0,0 +1,9 @@
+﻿using Converters.Domain.Conditions;
+
+namespace Converters.Web.Models.Convertations;
+
+public class DownloadConvertationDto
+{
+    public Guid Id { get; init; }
+    public FileType Type { get; init; }
+}
