49e57831219e417d234c0623e2ea606b5d5b296f
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-02 13:13:42 +0300

    add translations

---
+++ src/Converters/Converters.Web/Services/Translations/ConvertationToClientConvertationDtoMap.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Converters.Domain.Conditions;
+using Converters.Domain.Entities;
+using Converters.Web.Controllers;
+using Converters.Web.Models.Convertations;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Converters.Web.Services.Translations;
+
+public class ConvertationToClientConvertationDtoMap : Profile
+{
+    public ConvertationToClientConvertationDtoMap()
+    {
+        CreateMap<Convertation, ClientConvertationDto>()
+            .ForMember(x => x.Id, o => o.MapFrom(x => x.Id))
+            .ForMember(x => x.Created, o => o.MapFrom(x => x.Created.ToString("g")))
+            .ForMember(x => x.Name, o => o.MapFrom(x => x.Name))
+            .ForMember(x => x.JsonDownloadPath, o => o.MapFrom(x => GetLink(x.Id, FileType.Json)))
+            .ForMember(x => x.XmlDownloadPath, o => o.MapFrom(x => GetLink(x.Id, FileType.Xml)));
+    }
+    
+    private string GetLink(Guid id, FileType type)
+    {
+        return $"/download/?id={id}&type={type}";
+    }
+}
---
+++ src/Converters/Converters.Web/Services/Translations/ConvertationToGetConvertationDtoMap.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Converters.Domain.Entities;
+using Converters.Web.Models.Convertations;
+
+namespace Converters.Web.Services.Translations;
+
+public class ConvertationToGetConvertationDtoMap : Profile
+{
+    public ConvertationToGetConvertationDtoMap()
+    {
+        CreateMap<Convertation, GetConvertationDto>()
+            .ForMember(x => x.Id, o => o.MapFrom(x => x.Id))
+            .ForMember(x => x.Created, o => o.MapFrom(x => x.Created))
+            .ForMember(x => x.Name, o => o.MapFrom(x => x.Name))
+            .ForMember(x => x.JsonDownloadPath, o => o.Ignore())
+            .ForMember(x => x.XmlDownloadPath, o => o.Ignore());
+    }
+}
